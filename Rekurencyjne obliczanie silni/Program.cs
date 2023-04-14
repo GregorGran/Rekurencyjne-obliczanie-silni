e8928f668beda0573164b7b0a325523b817d80fb
Giorgio Armani <Giorgio Armani@DESKTOP-3ALTTPJ>
2023-04-14 13:43:44 +0200

    Add project files.

---
+++ Rekurencyjne obliczanie silni/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO.Enumeration;
+
+static int Silnia(int x)
+{
+    if (x < 1)
+    {
+        return 0;
+    }
+    else if (x == 1) 
+    {
+        return 1;
+    }
+    else
+    {
+        return x * Silnia(x - 1);
+    }
+}
+static void ObliczSilnie()
+{
+    for (int i = 1; i < 15; i++)
+    {
+        Console.WriteLine($"{i}!= {Silnia(i)}");
+    }
+}
+ObliczSilnie();
